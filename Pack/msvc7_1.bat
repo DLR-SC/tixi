9fcdeda4e334fdb4121f6e242ef948fe5d2f4fd6
martinsiggel@gmail.com <martinsiggel@gmail.com>
2012-08-09 09:28:33 +0000

    further directory changes

--- Pack/msvc7_1.bat
+++
@@ -1,5 +0,0 @@
-set FLAGS="/Od  /D WIN32 /D NDEBUG /D _LIB /D FORTRAN /D IFORT_WIN32 /D _MBCS /MLd /Za  /c  /TC"
-set LIBXML2_HOME=C:\DLR\TIVA\local
-set MSVCVersion=9.0
-set RT=ML
-(scons Pack=yes Tool=msvc MSVSVersion=%MSVCVersion% FC=ifort -c . & scons Pack=yes  Tool=msvc MSVSVersion=%MSVCVersion%  FC=ifort CCFLAGS=%FLAGS% ) & ( del tixi_win32_msvc7.1_ifort_%RT%.zip & ren tixi_win32_msvc7.1_ifort.zip tixi_win32_msvc7.1_ifort_%RT%.zip)
