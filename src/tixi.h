a6d6e15dbe2cb91347f10806350b07625462240c
martinsiggel@gmail.com <martinsiggel@gmail.com>
2012-08-10 07:35:30 +0000

    - changed to the cmake way for dealing with exports

--- src/tixi.h
+++ src/tixi.h
@@ -22,10 +22,14 @@
 extern "C" {
 #endif
 
-#ifdef WIN32_DLL
-#   define DLL_EXPORT __declspec (dllexport)
+#if defined(WIN32)
+  #if defined (TIXI_EXPORTS)
+    #define DLL_EXPORT __declspec (dllexport)
+  #else
+    #define DLL_EXPORT __declspec (dllimport)
+  #endif
 #else
-#   define DLL_EXPORT
+    #define DLL_EXPORT
 #endif
 
 /* \mainpage TIXI Manual
