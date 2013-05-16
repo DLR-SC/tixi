80bd2071794e485490734fbcb20cf5d445cff3d9
Markus Litz <markus.litz@dlr.de>
2013-05-16 09:55:58 +0200

    Finished valgrind check implementation.

--- contrib/Valgrind.cmake
+++ contrib/Valgrind.cmake
@@ -4,7 +4,6 @@ FUNCTION(SETUP_TARGET_FOR_VALGRIND _targetname _testbinname _outputname)
 
     # Check prereqs
     FIND_PROGRAM( VALGRIND_PATH valgrind )
-    MESSAGE(STATUS "${VALGRIND_PATH")
     
     # add test case
     if(USE_VALGRIND)
@@ -12,17 +11,18 @@ FUNCTION(SETUP_TARGET_FOR_VALGRIND _targetname _testbinname _outputname)
             if(NOT MSVC)
                     # add valgrind as a custom target
                     add_custom_target( ${_targetname} 
-                            COMMAND valgrind
+                            COMMAND ${VALGRIND_PATH}
                                     --leak-check=full
                                     --show-reachable=no
                                     --track-fds=yes
-                                    --error-exitcode=1
+                                    --error-exitcode=0
                                     --track-origins=yes
                                     --log-file=${CMAKE_CURRENT_BINARY_DIR}/${_outputname}.log 
                                     --xml=yes --xml-file=${CMAKE_CURRENT_BINARY_DIR}/${_outputname}.xml 
                                     ${CMAKE_CURRENT_BINARY_DIR}/tests/${_testbinname}
                             WORKING_DIRECTORY
                                     ${CMAKE_CURRENT_BINARY_DIR}/tests
+                            COMMENT "Valgrind report will be saved to ${CMAKE_CURRENT_BINARY_DIR}/${_outputname}.[log|xml]" 
                     )
             endif(NOT MSVC)
     endif(USE_VALGRIND)
