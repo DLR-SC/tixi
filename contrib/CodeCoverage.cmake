987bd24e51139bf1fb8fe6dee414bc635cc789b2
Martin Siggel <martin.siggel@dlr.de>
2013-05-16 14:40:11 +0200

    removed thirdparty directory from cobertura coverage report

--- contrib/CodeCoverage.cmake
+++ contrib/CodeCoverage.cmake
@@ -105,7 +105,7 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE_COBERTURA _targetname _testrunner _outputname
                 ${_testrunner} ${ARGV3}
 
                 # Running gcovr
-                COMMAND ${GCOVR_PATH} -x -r ${CMAKE_SOURCE_DIR} -e '${CMAKE_SOURCE_DIR}/tests/'  -o ${_outputname}.xml
+                COMMAND ${GCOVR_PATH} -x -r ${CMAKE_SOURCE_DIR} -e '${CMAKE_SOURCE_DIR}/thirdparty/' -e '${CMAKE_SOURCE_DIR}/tests/'  -o ${_outputname}.xml
                 WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                 COMMENT "Running gcovr to produce Cobertura code coverage report."
         )
