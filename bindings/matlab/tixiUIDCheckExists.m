27e73a29dc5e938d778e60f080854e8fe1ef8de5
Martin Siggel <martin.siggel@dlr.de>
2014-03-04 13:07:01 +0100

    Added custom tixiUIDCheckExists.m MATLAB wrapper (reverts back old behavior)

---
+++ bindings/matlab/tixiUIDCheckExists.m
@@ -0,0 +1,21 @@
+function tixiUIDCheckExists(handle, uID)
+    if (ischar(handle))
+        error('Invalid type of argument "handle"');
+    end
+
+    if not(ischar(uID))
+        error('Invalid type of argument "uID"');
+    end
+
+    try
+        tixi_matlab('tixiUIDCheckExists', handle, uID);
+        found = true;
+    catch err
+        if strfind(err.message, 'returned code 32') || strfind(err.message, 'returned code 33')
+            found = false;
+        else
+            error(err.message);
+        end
+    end
+end
+
