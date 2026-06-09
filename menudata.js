1f68da1ad7a42ec2784e2604a52518755ef2227b
Jan Kleinert <jan.kleinert@dlr.de>
2026-06-09 12:34:44 +0200

    small update to documentation

--- menudata.js
+++ menudata.js
@@ -28,7 +28,7 @@ var menudata={children:[
 {text:"Topics",url:"topics.html"},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
-{text:"File Members",url:"globals.html",children:[
+{text:"Globals",url:"globals.html",children:[
 {text:"All",url:"globals.html",children:[
 {text:"a",url:"globals.html#index_a"},
 {text:"c",url:"globals.html#index_c"},
--- navtreedata.js
+++ navtreedata.js
@@ -60,7 +60,7 @@ var NAVTREE =
     [ "Topics", "topics.html", "topics" ],
     [ "Files", "files.html", [
       [ "File List", "files.html", "files_dup" ],
-      [ "File Members", "globals.html", [
+      [ "Globals", "globals.html", [
         [ "All", "globals.html", null ],
         [ "Functions", "globals_func.html", null ],
         [ "Typedefs", "globals_type.html", null ],
